1e90e12919bbf7f809f481d9dc8e8240ef5e2398
Logan <logan.fries@uconn.edu>
2023-04-19 22:43:33 -0400

    adds constructor for Book.

--- script.js
+++ script.js
@@ -2,4 +2,14 @@
 
 let myLibrary = [];
 
-function Book() {}
+function Book(title, author, numberOfPages, hasRead) {
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.hasRead = hasRead;
+  this.info = function () {
+    return `${title} by ${author}, ${numberOfPages} pages, ${
+      hasRead ? "has read" : "has not read"
+    }.`;
+  };
+}
