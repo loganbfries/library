ea9bd18eae93ebddb11aba69cef5ddce883da5ae
Logan <logan.fries@uconn.edu>
2023-04-21 22:46:36 -0400

    adds untested function for displaying book with some commented out code for testing.

--- script.js
+++ script.js
@@ -25,3 +25,34 @@ function Book(title, author, numberOfPages, hasRead) {
 function addsBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+function displayBookCard(book) {
+  const card = document.createElement("div");
+  card.classList = "card-body";
+
+  const content = `
+    <div class = 'card'> 
+      <h4>${book.title}</h4>
+      <p>By: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+    </div>
+  `;
+}
+
+// // function addNewBook()
+
+// const Hobbit = new Book("Hobbit", "J.R.R. Tolkein", 400, true);
+// addsBookToLibrary(Hobbit);
+
+// const Rust = new Book(
+//   "The Rust Programming Language",
+//   "Steve Klabnik",
+//   600,
+//   false
+// );
+// addsBookToLibrary(Rust);
+
+// console.log(myLibrary);
+// console.log(typeof myLibrary);
+
+// myLibrary.forEach((item) => console.log(item.title));
